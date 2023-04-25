dae076dcabb27544593a0d9ea41c38b7bcb8d4e6
adityank11 <kelkaradityan17@gmail.com>
2023-04-25 11:44:07 +0530

    v5

---
+++ strings/basics.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int getlen(char arr[])
+{
+    int count = 0;
+    for (int i = 0; arr[i] !='\0'; i++)
+    {
+       count++;
+    }
+   return count;  
+}
+
+int main()
+{
+    char name[20];
+    // cout<<"Enter name: ";
+    // cin>>name;
+    // cout<<"Your name is: "<<name<<endl;
+    // cout<<"Length: "<<getlen(name);
+    string k = "Aditya";
+    // k.replace("a","c");
+    cout<<k;
+    return 0;
+}
